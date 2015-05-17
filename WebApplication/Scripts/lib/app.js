fdf710cfd9a7f33ab3e38d35140ce0ecc3726e15
hircjusz <darek.uchman@gmail.com>
2015-05-17 15:28:58 +0200

    odd -even

--- WebApplication/Scripts/lib/app.js
+++ WebApplication/Scripts/lib/app.js
@@ -5,7 +5,14 @@
 //};
 
 app.controller('MainController', function($scope) {
-    $scope.val = 'test456';
+    $scope.val = 1;
+    $scope.even = false;
+
+    $scope.inc = function () {
+        $scope.val += 1;
+        $scope.even= $scope.val % 2 == 0;
+    };
+
 });
 //angular.module('controllers', []).controller('MainController',
 //    function($scope) {
