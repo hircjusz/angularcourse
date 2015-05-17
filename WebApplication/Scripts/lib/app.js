007219be1b0c6df532f28f68d40bb42256c6c8f2
hircjusz <darek.uchman@gmail.com>
2015-05-17 12:46:19 +0200

    controoler

--- WebApplication/Scripts/lib/app.js
+++ WebApplication/Scripts/lib/app.js
@@ -1,1 +1,14 @@
-﻿angular.module('app', []);
+﻿var app=angular.module('app', []);
+
+//var MainController = function ($scope) {
+//    $scope.val = "test123";
+//};
+
+app.controller('MainController', function($scope) {
+    $scope.val = 'test456';
+});
+//angular.module('controllers', []).controller('MainController',
+//    function($scope) {
+//        $scope.val = 'test789';
+//    });
+
