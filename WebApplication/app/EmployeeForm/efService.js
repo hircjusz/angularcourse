771b4c0535927df23d259feb3410eb29e9d46281
hircjusz <darek.uchman@gmail.com>
2015-05-23 21:49:36 +0200

    displaying our form

---
+++ WebApplication/app/EmployeeForm/efService.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="efService.js" />
+angularFormsApp.factory('efService', function () {
+    return {
+        employee: {
+            fullName: 'Milton Waddams',
+            notes: "The ideal employee",
+            department: "Adinistration",
+            perkCar: true,
+            perkStock: false,
+            perkSixWeeks: true,
+            payrollType: "none"
+        }
+    };
+});
